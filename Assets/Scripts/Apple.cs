3ba584b9b2a757c5ec0f54bd9e0fe37da8fbb1ed
Dmytro Nasyrov <dmytro@pharosproduction.com>
2019-04-20 22:30:55 +0300

    add collisions

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+
+    public float velocity = 1;
+    private Vector3 _direction;
+
+    void Start()
+    {
+        float randomValue = Random.Range(-1.0f, 1.0f);
+        float x = Mathf.Sign(randomValue);
+        _direction = new Vector3(x, 1.0f, 0.0f);
+        _direction.Normalize();
+    }
+
+    void Update()
+    {
+        transform.position += _direction * velocity * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _direction = Vector3.Reflect(_direction, collision.contacts[0].normal);
+        _direction.Normalize();
+    }
+}
---
+++ Assets/Scripts/ColliderCreator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderCreator : MonoBehaviour
+{
+
+    public Camera cam;
+    public EdgeCollider2D edgeCollider;
+
+    void Start()
+    {
+        Vector3 bottomLeft = cam.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 topLeft = cam.ScreenToWorldPoint(new Vector3(0, Screen.height, 0));
+        Vector3 topRight = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        Vector3 bottomRight = cam.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0));
+        edgeCollider.points = new Vector2[5] { bottomLeft, topLeft, topRight, bottomRight, bottomLeft };
+    }
+
+    void Update()
+    {
+
+    }
+}
